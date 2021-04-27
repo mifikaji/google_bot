17370026328f40e78ed20ae7f89a20afaae6708d
mifikaji <82213199+mifikaji@users.noreply.github.com>
2021-04-27 12:22:36 +0300

    Обновление
    1. Переходит на целевой сайт и ходит по ссылкам, с вероятностью 30% переходит обратно в гугл
    2. Вбивает фразу по буквам
    3. Имеет ограничение на глубину поиска не далее 5 страницы

--- google_bot.js
+++ google_bot.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
+// @match        https://napli.ru/*
 // @icon         
 // @grant        none
 // ==/UserScript==
@@ -13,20 +14,58 @@ let keywords=["вывод произвольных полей wordpress", "10 с
 let btnK=document.getElementsByName('btnK')[0];
 let links=document.links;
 let keyword=keywords[getRandom(0,keywords.length)];
+let googleInput=document.getElementsByName('q')[0];
+let i=0;
 
 if(btnK!==undefined){
-    document.getElementsByName('q')[0].value=keyword;
-    document.getElementsByName('btnK')[0].click();
-}else{
+    let timerId=setInterval(function(){
+        googleInput.value+=keyword[i];
+        i++;
+        if(i==keyword.length){
+            clearInterval(timerId);
+            btnK.click();
+        }
+    },1000);
+}else if(location.hostname=="napli.ru"){
+    console.log("Мы на месте");
+    setTimeout(()=>{
+        let index=getRandom(0,links.length);
+        if(getRandom(0,101)>=70){
+        location.href="https://www.google.com/";
+        }
+        if(links[index].href.indexOf('napli.ru')!=-1){
+            links[index].click();
+        }
+    },getRandom(3500,7000));
+}
+else{
+    let nextGooglePage=true;
     for (let i=0; i<links.length; i++){
         if(links[i].href.indexOf('napli.ru')!=-1){
             let link=links[i];
+            nextGooglePage=false;
             console.log("Нашёл фразу " + links[i]);
-            link.click();
+            setTimeout(()=>{
+                link.click();
+            },getRandom(1000,4500));
             break;
         }
+    }
+    if(document.querySelector(".YyVfkd").innerText=="5"){
+        nextGooglePage=false;
+        location.href="https://www.google.com"
+    }
+
+    if(document.querySelector(".YyVfkd").innerText!=="5"){
+        setTimeout(()=>{
+            pnnext.click();
+        },getRandom(3000,5500));
     }
 }
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
+
+
+
+
